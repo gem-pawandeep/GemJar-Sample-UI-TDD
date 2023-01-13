af6dc9ab821b7219ba032c4584f6135e1ade104b
Pawan.Deep <pawan.deep@geminisolutions.com>
2023-01-13 12:18:15 +0530

    FINAL COMMIT WITH SINGLE AND PARALLEL TESTCASES EXECUTION REPORTS.

--- src/main/java/com/qa/GemJar_UI_Sample/Pages/ToolsQA.java
+++ src/main/java/com/qa/GemJar_UI_Sample/Pages/ToolsQA.java
@@ -10,9 +10,6 @@ public class ToolsQA {
 
     public static void fillForm(String firstName, String lastName, String email, String gender, String mobile, String month, String year, String path, String address) throws IOException {
         //code
-        DriverAction.setImplicitTimeOut(100);
-        DriverAction.setScriptTimeOut(100);
-        DriverAction.setPageLoadTimeOut(100);
         DriverAction.pageScroll(200, 300);
         DriverAction.waitSec(2);
         DriverAction.click(ToolsQA_locators.Forms, "Forms");
@@ -22,9 +19,9 @@ public class ToolsQA {
         DriverAction.waitSec(2);
         DriverAction.pageScroll(0, 300);
         DriverAction.typeText(ToolsQA_locators.firstName, firstName, "first name");
-        DriverAction.waitSec(2);
+        DriverAction.waitUntilElementAppear(ToolsQA_locators.lastName,20);
         DriverAction.typeText(ToolsQA_locators.lastName, lastName, "last name");
-        DriverAction.waitSec(2);
+        DriverAction.waitUntilElementAppear(ToolsQA_locators.email,20);
         DriverAction.typeText(ToolsQA_locators.email, email, "Email");
         DriverAction.waitSec(2);
         if (gender.equals("male")) {
@@ -37,25 +34,24 @@ public class ToolsQA {
         DriverAction.waitSec(2);
         DriverAction.pageScroll(0, 300);
         DriverAction.typeText(ToolsQA_locators.mobileNo, mobile, "Mobile");
-        DriverAction.waitSec(2);
+        DriverAction.waitUntilElementAppear(ToolsQA_locators.dob,20);
         DriverAction.click(ToolsQA_locators.dob, "Date of Birth");
-        DriverAction.waitSec(2);
+        DriverAction.waitUntilElementAppear(ToolsQA_locators.month,20);
         DriverAction.dropDown(ToolsQA_locators.month, month);//July
-        DriverAction.waitSec(2);
+        DriverAction.waitUntilElementAppear(ToolsQA_locators.years,20);
         DriverAction.dropDown(ToolsQA_locators.years, year); //2000
-        DriverAction.waitSec(2);
+        DriverAction.waitUntilElementAppear(ToolsQA_locators.date,20);
         DriverAction.click(ToolsQA_locators.date, "1");
-        DriverAction.waitSec(2);
+        DriverAction.waitUntilElementAppear(ToolsQA_locators.sports,20);
         DriverAction.click(ToolsQA_locators.sports, "sports");
-        DriverAction.waitSec(2);
+        DriverAction.waitUntilElementAppear(ToolsQA_locators.reading,20);
         DriverAction.click(ToolsQA_locators.reading, "reading");
-        DriverAction.waitSec(2);
+        DriverAction.waitUntilElementAppear(ToolsQA_locators.music,20);
         DriverAction.click(ToolsQA_locators.music, "music");
-        DriverAction.waitSec(2);
+        DriverAction.waitUntilElementAppear(ToolsQA_locators.upload,20);
         DriverAction.fileUpload(ToolsQA_locators.upload, path);
-        DriverAction.waitSec(2);
+        DriverAction.waitUntilElementAppear(ToolsQA_locators.address,20);
         DriverAction.typeText(ToolsQA_locators.address, address, "Address");
-        DriverAction.waitSec(2);
     }
 
 }
